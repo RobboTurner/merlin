b7c813a3194af5ab422a638371f0e6a2e0e85f61
RobboTurner <rob.turner97@outlook.com>
2026-01-10 19:06:08 +0000

    Revert "Adding audio option when no names"
    This reverts commit 028b6f0e0c770b72268c51c7da395b1afbb51cb7.

--- src/audio.py
+++ src/audio.py
@@ -9,11 +9,5 @@ def read_list_aloud(list_to_read: list, shuffle_list = True) -> None:
         shuffle(list_to_read)
     
     sound_file = BytesIO()
-
-    if len(list_to_read) == 0:
-        speech = "No names submitted yet"
-    else:
-        speech = f"Prepare to hear your names. They are: {list_to_read}"
-
-    gTTS(speech, lang='en', tld="co.uk").write_to_fp(sound_file)
+    gTTS(f"Prepare to hear your names. They are: {list_to_read}", lang='en', tld="co.uk").write_to_fp(sound_file)
     st.audio(sound_file, autoplay = True)
