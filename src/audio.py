15a36843bafb6db005fb6640443c1de455db30ea
RobboTurner <rob.turner97@outlook.com>
2026-01-10 17:44:00 +0000

    british english (again)

--- src/audio.py
+++ src/audio.py
@@ -9,5 +9,5 @@ def read_list_aloud(list_to_read: list, shuffle_list = True) -> None:
         shuffle(list_to_read)
     
     sound_file = BytesIO()
-    gTTS(f"Prepare to hear your names. They are: {list_to_read}", lang='en').write_to_fp(sound_file)
-    st.audio(sound_file, autoplay = True, tld="co.uk")
+    gTTS(f"Prepare to hear your names. They are: {list_to_read}", lang='en', tld="co.uk").write_to_fp(sound_file)
+    st.audio(sound_file, autoplay = True)
